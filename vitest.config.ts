427e8ff81f61c0e03665b58da2bbfeb68c34983b
Wix Remote Machine <support@wix.com>
2026-08-13 23:54:39 +0000

    Manual code changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   // @ts-expect-error - vite-tsconfig-paths is not typed correctly
@@ -8,6 +8,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
+    include: ['src/**/*.{test,spec}.{ts,tsx}', 'src/**/__tests__/**/*.{ts,tsx}'],
   },
   esbuild: {
     target: 'node20',
