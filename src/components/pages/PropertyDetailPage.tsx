cccc0bc4ef1736295a67205761e597b1ca808c88
Wix Remote Machine <support@wix.com>
2026-08-16 02:27:52 +0000

    Manual code changes

--- src/components/pages/PropertyDetailPage.tsx
+++ src/components/pages/PropertyDetailPage.tsx
@@ -421,7 +421,7 @@ export default function PropertyDetailPage() {
                             animate={{ scale: 1, opacity: 1 }}
                             exit={{ scale: 0.95, opacity: 0 }}
                             transition={{ duration: 0.3 }}
-                            className="relative w-full max-w-6xl"
+                            className="relative"
                             onClick={(e) => e.stopPropagation()}
                           >
                             {/* Media Container */}
