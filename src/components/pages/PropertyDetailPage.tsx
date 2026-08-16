91f11f11f8cf66b81bca61f064fc37f82e47fc5e
Wix Remote Machine <support@wix.com>
2026-08-16 02:28:36 +0000

    Manual code changes

--- src/components/pages/PropertyDetailPage.tsx
+++ src/components/pages/PropertyDetailPage.tsx
@@ -432,7 +432,7 @@ export default function PropertyDetailPage() {
                                 animate={{ opacity: 1 }}
                                 exit={{ opacity: 0 }}
                                 transition={{ duration: 0.4 }}
-                                className="h-full flex items-center justify-center overflow-auto"
+                                className="flex items-center justify-center overflow-auto"
                               >
                                 {(() => {
                                   const selectedMedia = property.galeriaDeFotos[selectedImageIndex];
